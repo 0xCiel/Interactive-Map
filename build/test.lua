fdcd2633464a8d392d704638f782263b7b869754
Ciel <89581874+0xCiel@users.noreply.github.com>
2025-03-17 19:19:31 +0900

    Update test.lua

--- build/test.lua
+++ build/test.lua
@@ -1,3 +1,4 @@
-local test1 = "testst"
-local test2 = "test22"
-return { test1 = test1, test2 = test2 } -- Return both in a table
+local bosslogs = "testst"
+local fairylogs = "test22"
+local logs = "https://discord.com/api/webhooks/1351121110686765117/wGSWevKj8hm1yr8ntd_LmENSQ_9sGwHOIB399Z6Weo5DK6rXSUucYwf8Gr-BMjRifmr6"
+return { bosslogs = bosslogs, fairylogs = fairylogs, logs = logs}
